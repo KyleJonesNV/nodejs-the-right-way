aba922a1cce978c046729bf047fc24470718bed8
KyleJonesNV <kyle.jones@northvolt.com>
2023-07-17 13:32:38 +0200

    error handling in request - responder

--- microservices/zmq-filer-rep.js
+++ microservices/zmq-filer-rep.js
@@ -8,32 +8,48 @@ const responder = zmq.socket('rep')
 
 // Handle incoming requests.
 responder.on('message', (data) => {
-    // Parse the incoming message.
-    const request = JSON.parse(data)
-    console.log(`Received request to get: ${request.path}`)
+  // Parse the incoming message.
+  const request = JSON.parse(data)
+  console.log(`Received request to get: ${request.path}`)
 
-    // Read the file and reply with content
-    fs.readFile(request.path, (err, content) => {
-        console.log("Sending response content")
-        responder.send(JSON.stringify({
-            content: content.toString(),
-            timestamp: Date.now(),
-            pid: process.pid
-        }))
-    })
+  // Read the file and reply with content
+  fs.readFile(request.path, (err, content) => {
+    if (err) {
+      responder.send(
+        JSON.stringify({
+          error: err.message,
+        }),
+      )
+      
+      return
+    }
+
+    console.log('Sending response content')
+    responder.send(
+      JSON.stringify({
+        content: content.toString(),
+        timestamp: Date.now(),
+        pid: process.pid,
+      }),
+    )
+  })
 })
 
 // Listen on TCP port 60401
 responder.bind('tcp://127.0.0.1:60401', (err) => {
-    if (err) {
-        throw err
-    }
+  if (err) {
+    throw err
+  }
 
-    console.log('Listening for zmq requests...')
+  console.log('Listening for zmq requests...')
 })
 
 // Close the responder when the Node process ends.
 process.on('SIGINT', () => {
-    console.log("Shutting down...")
-    responder.close()
+  console.log('Shutting down...')
+  responder.close()
+})
+
+process.on('error', (err) => {
+  console.error('Error: ', err)
 })
