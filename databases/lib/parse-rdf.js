a29fb959873883102e5cd1f991a23b245776e345
KyleJonesNV <kyle.jones@northvolt.com>
2023-07-17 18:44:48 +0200

    (BDD) implement functionality for tests

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -1,4 +1,25 @@
+'use strict'
+
+const cheerio = require('cheerio')
+
 module.exports = (rdf) => {
+  const $ = cheerio.load(rdf)
+
   const book = {}
+
+  book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '')
+  book.title = $('dcterms\\:title').text()
+  book.authors = $('pgterms\\:agent pgterms\\:name')
+    .toArray()
+    .map((element) => $(element).text())
+    
+  // Brackers introduce a css attribute selector and the '$=' indicates that 
+  // we want elements whose 'rdf:resource' attribute ends with '/LCSH'
+  book.subjects = $('[rdf\\:resource$="/LCSH"]')
+    .parent()
+    .find('rdf\\:value')
+    .toArray()
+    .map((element) => $(element).text())
+
   return book
 }
